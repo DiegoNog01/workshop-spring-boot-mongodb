37ce1c7993722027a0673060c41541c6c683b818
Diego Nogueira <mayconzkm@hotmail.com>
2024-04-01 09:27:15 -0300

    Getting a user by id

--- src/main/java/com/diegonogueira/workshopmongo/services/UserService.java
+++ src/main/java/com/diegonogueira/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.diegonogueira.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diegonogueira.workshopmongo.domain.User;
 import com.diegonogueira.workshopmongo.repository.UserRepository;
+import com.diegonogueira.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> optionalUser = repo.findById(id);
+		User user = optionalUser.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		return user;
 	}
 }
